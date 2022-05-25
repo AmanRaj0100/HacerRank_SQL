ac0bff8c024a61a216d22750cfba56d8a346da37
AmanRaj0100 <105361574+AmanRaj0100@users.noreply.github.com>
2022-05-25 20:43:55 +0530

    Create weather_observation_station_5.sql

---
+++ Basic_Select/weather_observation_station_5.sql
@@ -0,0 +1,7 @@
+# Author: Aman Srivastava
+select city,length(city) 
+from station
+order by length(city) asc, city asc limit 1;
+select city,length(city)
+from station
+order by length(city) desc, city asc limit 1;
